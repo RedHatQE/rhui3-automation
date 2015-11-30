00d3b0350d76773093384e2358824060159f6f4b
Irina Gulina <igulina@redhat.com>
2015-11-30 19:32:46 +0100

    Add __init__.py for nosetests and 2 dummy tests

--- tests/rhui3_tests/test_example.py
+++ tests/rhui3_tests/test_example.py
@@ -1,16 +1,24 @@
 #! /usr/bin/python -tt
 
-import nose
+import nose, stitches, logging
 
 from rhui3_tests_lib.rhui_testcase import *
 from rhui3_tests_lib.rhuimanager import *
+from rhui3_tests_lib.rhuimanager_cds import RHUIManagerCds
 
-class test_example(RHUITestcase):
-  def _setup(self):
+logging.basicConfig(level=logging.DEBUG)
+
+connection=stitches.connection.Connection("rhua.example.com", "root", "/root/.ssh/id_rsa_master")
+
+
+def test_1():
+    ''' dummy test to pass'''
     pass
 
-  def _test(self):
-    pass
+def test_2():
+    ''' dummy test to fail'''
+    assert True != False
 
-  def _cleanup(self):
-    pass
+def test_3():
+    cds_list=RHUIManagerCds.list(connection)
+    assert []==cds_list
