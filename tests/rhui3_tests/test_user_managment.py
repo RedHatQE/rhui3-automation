f4ea181e2c5b13166f8fde64a6cfc4db14548cce
Irina Gulina <igulina@redhat.com>
2016-01-17 04:13:24 +0100

    Change a file name

--- tests/rhui3_tests/test_user_managment.py
+++ tests/rhui3_tests/test_user_managment.py
@@ -10,7 +10,7 @@ logging.basicConfig(level=logging.DEBUG)
 
 connection=stitches.connection.Connection("rhua.example.com", "root", "/root/.ssh/id_rsa_master")
 
-with open('/tmp/rhui3-tests/tests/rhui3_tests/rhui_manager.cfg', 'r') as file:
+with open('/tmp/rhui3-tests/tests/rhui3_tests/rhui_manager.yaml', 'r') as file:
     doc = yaml.load(file)
 
 rhui_login = doc['rhui_login']
