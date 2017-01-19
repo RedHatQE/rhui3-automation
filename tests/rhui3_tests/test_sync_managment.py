5788d0d13acf1801450a5a3a27cb3c684bcc856a
Irina <alexxa@users.noreply.github.com>
2017-01-19 11:07:15 +0100

    Sync lib and tests (#35)
    * Sync lib and tests
    
    * Update according sync BZ fix

---
+++ tests/rhui3_tests/test_sync_managment.py
@@ -0,0 +1,49 @@
+import nose, stitches, yaml, time
+
+from rhui3_tests_lib.rhuimanager import *
+from rhui3_tests_lib.rhuimanager_repo import *
+from rhui3_tests_lib.rhuimanager_sync import *
+from rhui3_tests_lib.rhuimanager_entitlement import *
+
+from os.path import basename
+
+logging.basicConfig(level=logging.DEBUG)
+
+connection=stitches.connection.Connection("rhua.example.com", "root", "/root/.ssh/id_rsa_test")
+
+def setUp():
+    print "*** Running %s: *** " % basename(__file__)
+
+def test_01_setup():
+    '''do rhui-manager login, upload RH cert, add a repo to sync '''
+    RHUIManager.initial_run(connection)
+    RHUIManagerEntitlements.upload_rh_certificate(connection)
+    Expect.enter(connection, "home")
+    Expect.expect(connection, ".*rhui \(" + "home" + "\) =>")
+    RHUIManagerRepo.add_rh_repo_by_repo(connection, ["Red Hat Update Infrastructure 2.0 \(RPMs\) \(6Server-x86_64\) \(Yum\)"])
+    Expect.enter(connection, "home")
+    Expect.expect(connection, ".*rhui \(" + "home" + "\) =>")
+
+def test_02_sync_repo():
+    '''sync a RH repo '''
+    RHUIManagerSync.sync_repo(connection, ["Red Hat Update Infrastructure 2.0 \(RPMs\) \(6Server-x86_64\)"])
+
+def test_03_check_sync_status():
+    '''check sync status of the repo'''
+    Expect.ping_pong(connection, "rhui-manager status | grep \"Red Hat Update Infrastructure 2.0 (RPMs) (6Server-x86_64)\" | grep \"SUCCESS\|SYNCING\" && echo SUCCESS", "[^ ]SUCCESS", 120)
+
+def test_04_cleanup():
+    '''Wait until repo is synced and remove it then'''
+    reposync = ["", "", "Running"]
+    while reposync[2] == "Running" or reposync[2] == "Never":
+        time.sleep(10)
+        reposync = RHUIManagerSync.get_repo_status(connection, "Red Hat Update Infrastructure 2.0 \(RPMs\) \(6Server-x86_64\)")
+    nose.tools.assert_equal(reposync[2], "Success")
+
+    '''remove the RH repo '''
+    Expect.enter(connection, "home")
+    Expect.expect(connection, ".*rhui \(" + "home" + "\) =>")
+    RHUIManagerRepo.delete_repo(connection, ["Red Hat Update Infrastructure 2.0 \(RPMs\).*"])
+
+def tearDown():
+    print "*** Finished running %s. *** " % basename(__file__)
--- tests/rhui3_tests_lib/rhuimanager_sync.py
+++ tests/rhui3_tests_lib/rhuimanager_sync.py
@@ -12,44 +12,6 @@ class RHUIManagerSync(object):
     Represents -= Synchronization Status =- RHUI screen
     '''
     @staticmethod
-    def sync_cds(connection, cdslist):
-        '''
-        sync an individual CDS immediately
-        '''
-        RHUIManager.screen(connection, "sync")
-        Expect.enter(connection, "sc")
-        RHUIManager.select(connection, cdslist)
-        RHUIManager.proceed_with_check(connection, "The following CDS instances will be scheduled for synchronization:", cdslist)
-        RHUIManager.quit(connection)
-
-    @staticmethod
-    def sync_cluster(connection, clusterlist):
-        '''
-        sync a CDS cluster immediately
-        '''
-        RHUIManager.screen(connection, "sync")
-        Expect.enter(connection, "sl")
-        RHUIManager.select(connection, clusterlist)
-        RHUIManager.proceed_with_check(connection, "The following CDS clusters will be scheduled for synchronization:", clusterlist)
-        RHUIManager.quit(connection)
-
-    @staticmethod
-    def get_cds_status(connection, cdsname):
-        '''
-        display CDS sync summary
-        '''
-        RHUIManager.screen(connection, "sync")
-        Expect.enter(connection, "dc")
-        res_list = Expect.match(connection, re.compile(".*\n" + cdsname.replace(".", "\.") + "[\.\s]*\[([^\n]*)\].*" + cdsname.replace(".", "\.") + "\s*\r\n([^\n]*)\r\n", re.DOTALL), [1, 2], 60)
-        connection.cli.exec_command("killall -s SIGINT rhui-manager")
-        ret_list = []
-        for val in [res_list[0]] + res_list[1].split("             "):
-            val = Util.uncolorify(val.strip())
-            ret_list.append(val)
-        RHUIManager.quit(connection)
-        return ret_list
-
-    @staticmethod
     def sync_repo(connection, repolist):
         '''
         sync an individual repository immediately
@@ -60,7 +22,7 @@ class RHUIManagerSync(object):
         Expect.enter(connection, "l")
         RHUIManager.select(connection, repolist)
         RHUIManager.proceed_with_check(connection, "The following repositories will be scheduled for synchronization:", repolist)
-        RHUIManager.quit(connection)
+        Expect.expect(connection, ".*rhui \(" + "sync" + "\) =>")
 
     @staticmethod
     def get_repo_status(connection, reponame):
@@ -76,5 +38,7 @@ class RHUIManagerSync(object):
         ret_list = res.split("             ")
         for i in range(len(ret_list)):
             ret_list[i] = ret_list[i].strip()
-        RHUIManager.quit(connection)
+
+        Expect.enter(connection, '\x03')
+        Expect.expect(connection, ".*rhui \(" + "sync" + "\) =>")
         return ret_list
