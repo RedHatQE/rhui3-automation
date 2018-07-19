3680d275abc3852f08696de97401f8329f88973f
Radek Bíba <radek@redhat.com>
2018-07-19 11:04:22 +0200

    make pylint happy with the RHUI 3 repo and user tests

--- tests/rhui3_tests/test_rhui_3_repos.py
+++ tests/rhui3_tests/test_rhui_3_repos.py
@@ -1,13 +1,14 @@
 ''' RHUI 3 RPM availability tests'''
 
-import nose, stitches, logging
-from stitches.expect import Expect
-
 from os.path import basename
+
+import logging
+import stitches
+from stitches.expect import Expect
 
 logging.basicConfig(level=logging.DEBUG)
 
-connection=stitches.connection.Connection("rhua.example.com", "root", "/root/.ssh/id_rsa_test")
+CONNECTION = stitches.Connection("rhua.example.com", "root", "/root/.ssh/id_rsa_test")
 
 def setup():
     '''
@@ -19,19 +20,33 @@ def test_01_install_wget():
     '''
         make sure wget is installed on the RHUA
     '''
-    Expect.expect_retval(connection, "yum -y install wget", timeout=30)
+    Expect.expect_retval(CONNECTION, "yum -y install wget", timeout=30)
 
 def test_02_rhui_3_for_rhel_6_check():
     '''
         check if the RHUI 3 packages for RHEL 6 are available
     '''
-    Expect.expect_retval(connection, "test $(wget -q -O - --certificate /tmp/extra_rhui_files/rhcert.pem --ca-certificate /etc/rhsm/ca/redhat-uep.pem https://cdn.redhat.com/content/dist/rhel/rhui/server/6/6Server/x86_64/rhui/3/os/Packages/ | grep -c \"A HREF.*\.rpm\") -gt 90", timeout=15)
+    Expect.expect_retval(CONNECTION,
+                         "test $(wget -q -O - " +
+                         "--certificate /tmp/extra_rhui_files/rhcert.pem " +
+                         "--ca-certificate /etc/rhsm/ca/redhat-uep.pem " +
+                         "https://cdn.redhat.com/content/dist/rhel/rhui/server/" +
+                         "6/6Server/x86_64/rhui/3/os/Packages/ | " +
+                         r"grep -c 'A HREF.*\.rpm') -gt 90",
+                         timeout=15)
 
 def test_03_rhui_3_for_rhel_7_check():
     '''
         check if the RHUI 3 packages for RHEL 7 are available
     '''
-    Expect.expect_retval(connection, "test $(wget -q -O - --certificate /tmp/extra_rhui_files/rhcert.pem --ca-certificate /etc/rhsm/ca/redhat-uep.pem https://cdn.redhat.com/content/dist/rhel/rhui/server/7/7Server/x86_64/rhui/3/os/Packages/ | grep -c \"A HREF.*\.rpm\") -gt 90", timeout=15)
+    Expect.expect_retval(CONNECTION,
+                         "test $(wget -q -O - " +
+                         "--certificate /tmp/extra_rhui_files/rhcert.pem " +
+                         "--ca-certificate /etc/rhsm/ca/redhat-uep.pem " +
+                         "https://cdn.redhat.com/content/dist/rhel/rhui/server/" +
+                         "7/7Server/x86_64/rhui/3/os/Packages/ | " +
+                         r"grep -c 'A HREF.*\.rpm') -gt 90",
+                         timeout=15)
 
 def teardown():
     '''
--- tests/rhui3_tests/test_user_management.py
+++ tests/rhui3_tests/test_user_management.py
@@ -1,16 +1,16 @@
 '''User management tests'''
 
-#! /usr/bin/python -tt
-
-import nose, unittest, stitches, logging, yaml
-
-from rhui3_tests_lib.rhuimanager import *
-
 from os.path import basename
+
+import logging
+import stitches
+from stitches.expect import Expect
+
+from rhui3_tests_lib.rhuimanager import RHUIManager
 
 logging.basicConfig(level=logging.DEBUG)
 
-connection=stitches.connection.Connection("rhua.example.com", "root", "/root/.ssh/id_rsa_test")
+CONNECTION = stitches.Connection("rhua.example.com", "root", "/root/.ssh/id_rsa_test")
 
 def setup():
     '''
@@ -22,38 +22,39 @@ def test_01_initial_run():
     '''
         TODO a test case for an initial password
     '''
-    RHUIManager.initial_run(connection)
+    RHUIManager.initial_run(CONNECTION)
 
 def test_02_change_password():
     '''
         change a user's password (will log the user out automatically)
     '''
-    RHUIManager.screen(connection, "users")
-    RHUIManager.change_user_password(connection, password = "new_rhui_pass")
+    RHUIManager.screen(CONNECTION, "users")
+    RHUIManager.change_user_password(CONNECTION, password="new_rhui_pass")
 
 def test_03_login_with_new_pass():
     '''
        log in with a new password
     '''
-    RHUIManager.initial_run(connection, password = "new_rhui_pass")
+    RHUIManager.initial_run(CONNECTION, password="new_rhui_pass")
 
 def test_04():
     '''
         change a user's password back to the default one
     '''
-    RHUIManager.screen(connection, "users")
-    RHUIManager.change_user_password(connection)
+    RHUIManager.screen(CONNECTION, "users")
+    RHUIManager.change_user_password(CONNECTION)
 
-def test_05_login_with_wrong_password():
+def test_05_login_with_wrong_pass():
     '''
         BZ 1282522. Doing initial run with the wrong password.
     '''
-    Expect.enter(connection, "rhui-manager")
-    Expect.expect(connection, ".*RHUI Username:.*")
-    Expect.enter(connection, "admin")
-    Expect.expect(connection, "RHUI Password:")
-    Expect.enter(connection, "wrong_pass")
-    Expect.expect(connection, ".*Invalid login, please check the authentication credentials and try again.")
+    Expect.enter(CONNECTION, "rhui-manager")
+    Expect.expect(CONNECTION, ".*RHUI Username:.*")
+    Expect.enter(CONNECTION, "admin")
+    Expect.expect(CONNECTION, "RHUI Password:")
+    Expect.enter(CONNECTION, "wrong_pass")
+    Expect.expect(CONNECTION,
+                  ".*Invalid login, please check the authentication credentials and try again.")
 
 def teardown():
     '''
