3ff34b135ac56e5c3100225f3495a117a09f8fad
Radek Bíba <radek@redhat.com>
2019-06-14 15:26:53 +0200

    reuse extra ssh args in login2cloud.sh

--- scripts/login2cloud.sh
+++ scripts/login2cloud.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 # Use this script to log in to all (non-Atomic) hosts that comprise a RHUI cloud formation.
 #
-# The mandatory first argument is supposed to be the host_*.cfg file created by create-cf-stack.py.
-# The optional second argument can contain extra SSH arguments; typically a reverse tunnel
-# to a web server in a VPN where an unreleased RHEL Beta compose is available and is needed
-# while installing CDS and HAProxy nodes as part of RHUI tests. IOW, if these nodes are running
-# an unreleased RHEL version, you need to be logged in to them with the reverse port for the RHUI
-# tests to be able to install additional packages on them.
+# The only mandatory argument is supposed to be the host_*.cfg file created by create-cf-stack.py.
+# If the hosts file contains extra SSH arguments for Ansible, this script will reuse them.
 #
 # The `cssh' utility is required.
 if ! which cssh &> /dev/null; then
@@ -18,12 +14,13 @@ if [ "$1" ]; then
     file=$1
     hosts=$(awk '/ansible_ssh_user=ec2-user/ { print $1 }' $file | sort -u)
     key=$(grep -m 1 -o 'ansible_ssh_private_key_file=[^ ]*' $file | cut -d = -f 2)
+    ssh_extra_args=$(grep -m 1 -o 'ansible_ssh_extra_args="[^"]*"' $file | cut -d = -f 2 | sed 's/"//g')
   else
     echo "$1 is not a (hosts) file."
     exit 1
   fi
-  cssh -o "-i $key -l ec2-user $2" $hosts
+  cssh -o "-i $key -l ec2-user $ssh_extra_args" $hosts
 else
-  echo "Usage: $0 hosts_file [extra_ssh_arguments, e.g. '-R local_port:server_in_your_vpn:port']"
+  echo "Usage: $0 hosts_file"
   exit 1
 fi
